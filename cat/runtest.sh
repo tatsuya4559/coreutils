e7ea891f9d54733048ee92bfaca8c141d075f3a6
tatsuya4559 <tatsuya.k1029@gmail.com>
2021-09-10 08:49:07 +0900

    Improve test

--- cat/runtest.sh
+++ cat/runtest.sh
@@ -2,6 +2,11 @@
 set -e
 go build
 
+echo 'ls | cat'
 diff -u <(ls | cat) <(ls | ./cat)
+
+echo 'cat main.go'
 diff -u <(cat main.go) <(./cat main.go)
-diff -u <(./cat runtest.sh) <(./cat main.go) >/dev/null || true
+
+echo 'cat main.go runtest.sh'
+diff -u <(cat main.go runtest.sh) <(./cat main.go runtest.sh)
--- wc/runtest.sh
+++ wc/runtest.sh
@@ -2,8 +2,11 @@
 set -e
 go build
 
+echo 'wc main.go'
 diff -uw <(./wc main.go) <(wc main.go)
 
+echo 'wc main.go runtest.sh'
 diff -uw <(./wc main.go runtest.sh) <(wc main.go runtest.sh)
 
+echo 'wc main.go hoge'
 diff -uw <(./wc main.go hoge 2>&1) <(wc main.go hoge 2>&1)
